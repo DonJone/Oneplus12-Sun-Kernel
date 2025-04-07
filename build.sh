6d0d2b989f701e1b9bae0ea27f22f30470ae01b3
DonJone <111963278+DonJone@users.noreply.github.com>
2025-04-07 23:53:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ repo init -u https://github.com/RealJohnGalt/opsm8650_kernel_manifest.git -b kpl
 repo sync
 git clone https://github.com/TheWildJames/kernel_patches.git
 git clone https://gitlab.com/simonpunk/susfs4ksu -b gki-android14-6.1 --depth=1 susfs
+#git clone https://github.com/TheWildJames/kernel_patches.git
+#git clone https://gitlab.com/simonpunk/susfs4ksu -b gki-android15-6.6 --depth=1 susfs
 rm -rf /home/don/kernel_workspace/kernel_platform/common/android/abi_gki_protected_exports_*
 cd ~/kernel_workspace/kernel_platform/
 KERNEL_REPO=$(pwd)
@@ -13,10 +15,12 @@ sed -i 's/ -dirty//g' ~/kernel_workspace/kernel_platform/common/scripts/setlocal
 sed -i 's/ -dirty//g' ~/kernel_workspace/kernel_platform/msm-kernel/scripts/setlocalversion
 cd ~/kernel_workspace/susfs
 cp ./kernel_patches/50_add_susfs_in_gki-android14-6.1.patch $KERNEL_REPO/common/
+#cp ./kernel_patches/50_add_susfs_in_gki-android15-6.6.patch $KERNEL_REPO/common/
 cp ./kernel_patches/fs/* $KERNEL_REPO/common/fs/
 cp ./kernel_patches/include/linux/* $KERNEL_REPO/common/include/linux/
 cd $KERNEL_REPO/common
 patch -p1 < 50_add_susfs_in_gki-android14-6.1.patch
+#patch -p1 < 50_add_susfs_in_gki-android15-6.6.patch
 cp ../../kernel_patches/69_hide_stuff.patch ./
 patch -p1 -F 3 < 69_hide_stuff.patch
 cp ../../kernel_patches/hooks/new_hooks.patch ./
